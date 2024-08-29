32ad65e7cfe606392a2cdce5c0ce73f5e97d3f09
ybtuti <samuelmsuu@gmail.com>
2024-08-29 14:17:00 +0300

    feat: Add IDepositor interface for handling redemptions

---
+++ contracts/src/interface/IDepositor.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IDepositor {
+    function redeem(
+        address _from,
+        address _to,
+        address _token,
+        uint256 _amount
+    ) external;
+}
