eb0823cfba2876886126a60c2facfa64004d2024
ybtuti <samuelmsuu@gmail.com>
2024-08-31 07:34:00 +0300

    chore: Add DeployDSC contract for deploying DSC contract

---
+++ contracts/script/deploy/DeployDSC.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+import {DSC} from "../../src/DSC.sol";
+
+contract DeployDSC is Script {
+    function run() external {
+        // Start broadcasting transactions
+        vm.startBroadcast();
+
+        // Deploy the DSC contract
+        DSC dsc = new DSC();
+
+        // Stop broadcasting transactions
+        vm.stopBroadcast();
+    }
+}
