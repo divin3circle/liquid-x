569796fac07c008d3a9ae4856d8faf1814b5196c
ybtuti <samuelmsuu@gmail.com>
2024-09-01 13:56:41 +0300

    feat: Add DeployDepositor contract for deploying Depositor contract

--- contracts/script/deploy/DeployDepositor.s.sol
+++ contracts/script/deploy/DeployDepositor.s.sol
@@ -2,3 +2,28 @@
 pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
+import {Parameters} from "../Parameters.sol";
+import {Depositor} from "../../src/Depositor.sol";
+
+contract DeployDepositor is Script {
+    function run() external returns (Depositor depositor) {
+        // Retrieve chain configuration
+
+        ChainConfig.ChainComponent memory chainConfig = Parameters
+            .getChainConfig();
+
+        // Start broadcasting transactions
+        vm.startBroadcast();
+
+        // Deploy the Depositor contract
+        depositor = new Depositor(
+            chainConfig.chainSelector,
+            chainConfig.router,
+            Parameters.AVALANCHE_FUJI_CHAIN_SELECTOR,
+            Parameters.AVALANCHE_FUJI_MAIN_ROUTER
+        );
+
+        // Stop broadcasting transactions
+        vm.stopBroadcast();
+    }
+}
