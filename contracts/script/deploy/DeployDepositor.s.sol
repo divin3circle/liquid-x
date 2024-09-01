035b29b4bff2fadfbbefee529edd18dc186b731b
ybtuti <samuelmsuu@gmail.com>
2024-09-01 15:30:35 +0300

    feat: Update DeployDeposit contract to set allowed tokens and chains

--- contracts/script/deploy/DeployDepositor.s.sol
+++ contracts/script/deploy/DeployDepositor.s.sol
@@ -1,29 +1,49 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
 import {Parameters} from "../Parameters.sol";
-import {ChainConfig} from "../config/ChainConfig.s.sol";
+import {MainRouter} from "../../src/MainRouter.sol";
 import {Depositor} from "../../src/Depositor.sol";
+import {ChainConfig} from "../config/ChainConfig.s.sol";
+import {TokenConfig} from "../config/TokensConfig.s.sol";
 
-contract DeployDepositor is Script {
+contract DeployDeposit is Script, Parameters {
     function run() external returns (Depositor depositor) {
-        // Retrieve chain configuration
-        ChainConfig.ChainComponent memory chainConfig = Parameters
-            .getChainConfig();
+        ChainConfig config = new ChainConfig();
+        ChainConfig.ChainComponent memory component = config.run();
 
-        // Start broadcasting transactions
+        TokenConfig tokenConfig = new TokenConfig();
+        TokenConfig.Token memory token = tokenConfig.run();
+
+        address router = component.router;
+        uint64 chainSelector = component.chainSelector;
+
         vm.startBroadcast();
 
-        // Deploy the Depositor contract
         depositor = new Depositor(
-            chainConfig.chainSelector,
-            chainConfig.router,
-            Parameters.AVALANCHE_FUJI_CHAIN_SELECTOR,
-            Parameters.AVALANCHE_FUJI_MAIN_ROUTER
+            chainSelector,
+            router,
+            CHAIN_SELECTOR_OPTIMISM_SEPOLIA,
+            OPTIMISM_SEPOLIA_MAIN_ROUTER
         );
 
-        // Stop broadcasting transactions
+        depositor.setAllowedToken(token.wbtc.token, true);
+        depositor.setAllowedToken(token.weth.token, true);
+        depositor.setAllowedToken(token.link.token, true);
+        depositor.setAllowedToken(token.avax.token, true);
+        depositor.setAllowedToken(token.uni.token, true);
+        depositor.setAllowedToken(token.usdc.token, true);
+        depositor.setAllowedToken(token.usdt.token, true);
+
+        depositor.setAllowedDestinationChain(
+            CHAIN_SELECTOR_OPTIMISM_SEPOLIA,
+            true
+        );
+        depositor.setAllowedSourceChain(CHAIN_SELECTOR_OPTIMISM_SEPOLIA, true);
+        depositor.setAllowedSender(OPTIMISM_SEPOLIA_MAIN_ROUTER, true);
+
         vm.stopBroadcast();
     }
 }
