8cdd29f03df462180dedf4e9063bf0e3377a86a3
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-09-02 13:35:00 +0300

    refactor: Update launch button styling
    Refactor the launch button component to update its styling. Add rounded corners to the button and the background div to improve the visual design and consistency of the application.

--- frontend/components/app/buttons/launch-button.tsx
+++ frontend/components/app/buttons/launch-button.tsx
@@ -11,8 +11,8 @@ export function CustomButton({
 }) {
   return (
     // <ButtonsCard onClick={() => {}}>
-    <button className="px-8 py-2 border border-primary-500 bg-transparent text-black relative group transition duration-200">
-      <div className="absolute -bottom-2 -right-2 bg-primary-500 font-bold text-white h-full w-full z-10 group-hover:bottom-0 group-hover:right-0 transition-all duration-200" />
+    <button className="px-8 py-2 border border-primary-500 bg-transparent text-black relative group transition duration-200 rounded-md">
+      <div className="absolute -bottom-2 -right-2 bg-primary-500 font-bold text-white h-full w-full z-10 group-hover:bottom-0 group-hover:right-0 transition-all duration-200 rounded" />
       <span className="relative text-white font-bold z-10">{title}</span>
     </button>
     // </ButtonsCard>
