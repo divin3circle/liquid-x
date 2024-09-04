b828de6d01a7b001974134435de9f3a0319d6f79
ybtuti <samuelmsuu@gmail.com>
2024-09-04 15:49:17 +0300

    chore: Update DeployMinter script with Optimism Sepolia constants and addresses

--- contracts/script/deploy/DeployMinter.s.sol
+++ contracts/script/deploy/DeployMinter.s.sol
@@ -2,11 +2,11 @@
 
 pragma solidity ^0.8.0;
 
-import { Script } from "forge-std/Script.sol";
-import { Parameters } from "../Parameters.sol";
-import { MainRouter } from "../../src/MainRouter.sol";
-import { Minter } from "../../src/Minter.sol";
-import { ChainConfig } from "../config/ChainConfig.s.sol";
+import {Script} from "forge-std/Script.sol";
+import {Parameters} from "../Parameters.sol";
+import {MainRouter} from "../../src/MainRouter.sol";
+import {Minter} from "../../src/Minter.sol";
+import {ChainConfig} from "../config/ChainConfig.s.sol";
 
 contract DeployMinter is Script, Parameters {
     function run() external returns (Minter minter) {
@@ -17,11 +17,19 @@ contract DeployMinter is Script, Parameters {
         uint64 chainSelector = component.chainSelector;
 
         vm.startBroadcast();
-        minter = new Minter(chainSelector, router, AVALANCHE_FUJI_CHAIN_SELECTOR, AVALANCHE_FUJI_MAIN_ROUTER);
-        
-        minter.setAllowedDestinationChain(AVALANCHE_FUJI_CHAIN_SELECTOR, true);
-        minter.setAllowedSourceChain(AVALANCHE_FUJI_CHAIN_SELECTOR, true);
-        minter.setAllowedSender(AVALANCHE_FUJI_MAIN_ROUTER, true);
+        minter = new Minter(
+            chainSelector,
+            router,
+            CHAIN_SELECTOR_OPTIMISM_SEPOLIA,
+            OPTIMISM_SEPOLIA_MAIN_ROUTER
+        );
+
+        minter.setAllowedDestinationChain(
+            CHAIN_SELECTOR_OPTIMISM_SEPOLIA,
+            true
+        );
+        minter.setAllowedSourceChain(CHAIN_SELECTOR_OPTIMISM_SEPOLIA, true);
+        minter.setAllowedSender(OPTIMISM_SEPOLIA_MAIN_ROUTER, true);
 
         vm.stopBroadcast();
     }
