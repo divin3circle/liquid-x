56751a4643878f7798b1fe196e5dd9e60b3c1e8d
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-09-05 09:01:17 +0300

    feat: Add Depositphotos to cSpell words and update dashboard layout
    This commit adds "Depositphotos" to the cSpell words in the VS Code settings, allowing it to be recognized as a valid word. Additionally, it updates the dashboard layout in the frontend app to include a logo, navigation links, and icons for different sections. This improves the visual design and user experience of the dashboard.

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,5 +1,14 @@
 import type { Metadata } from "next";
 import { Titillium_Web } from "next/font/google";
+import logo from "../../public/logo.png";
+import Image from "next/image";
+import { MdSpaceDashboard } from "react-icons/md";
+import { SiDepositphotos } from "react-icons/si";
+import { IoIosSwap } from "react-icons/io";
+import { FaGear, FaSackDollar } from "react-icons/fa6";
+import Link from "next/link";
+import { RiMessage3Fill } from "react-icons/ri";
+import { IoLogOut } from "react-icons/io5";
 
 const titillium = Titillium_Web({
   weight: "400",
@@ -24,7 +33,79 @@ export default function DashboardLayout({
       </head>
       <body className={titillium.className}>
         <main className="flex">
-          <aside className="hidden md:block w-1/4">Sidebar</aside>
+          <aside className="hidden md:flex flex-col justify-between w-[15%] mt-2 bg-[##FAF9F6] shadow-md h-screen pl-2">
+            <div className="flex gap-2 items-center mx-4 my-4">
+              <Image
+                src={logo}
+                alt="LiquidX"
+                className=""
+                width={50}
+                height={50}
+              />
+              <h1 className="font-bold ">liquidX</h1>
+            </div>
+            <div>
+              <nav className="flex flex-col gap-2">
+                <Link
+                  href="/dashboard"
+                  className="p-4 flex gap-2 items-center my-2"
+                >
+                  <MdSpaceDashboard />
+                  Dashboard
+                </Link>
+                <Link
+                  href="/dashboard/swap"
+                  className="p-4 flex gap-2 items-center my-2"
+                >
+                  <SiDepositphotos />
+                  Deposit & Borrow
+                </Link>
+                <Link
+                  href="/dashboard/pool"
+                  className="p-4 flex gap-2 items-center my-2"
+                >
+                  <IoIosSwap />
+                  Swap
+                </Link>
+                <Link
+                  href="/dashboard/farm"
+                  className="p-4 flex gap-2 items-center my-2"
+                >
+                  <FaSackDollar />
+                  Quick Borrow
+                </Link>
+              </nav>
+            </div>
+            <div>
+              <nav className="flex flex-col gap-2">
+                <Link
+                  href="/dashboard"
+                  className="p-4 flex gap-2 items-center my-2"
+                >
+                  <RiMessage3Fill />
+                  Support
+                </Link>
+                <Link
+                  href="/dashboard/swap"
+                  className="p-4 flex gap-2 items-center my-2"
+                >
+                  <FaGear />
+                  Settings
+                </Link>
+              </nav>
+            </div>
+            <div>
+              <nav className="flex flex-col gap-2">
+                <Link
+                  href="/dashboard"
+                  className="p-4 flex gap-2 items-center my-2"
+                >
+                  <IoLogOut />
+                  Logout
+                </Link>
+              </nav>
+            </div>
+          </aside>
           {children}
         </main>
       </body>
