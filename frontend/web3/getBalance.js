8d9790a5a1d3ffecd1891ca11577b25166859b4c
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-09-10 23:28:49 +0300

    chore: add getBalance util function

--- frontend/web3/getBalance.js
+++ frontend/web3/getBalance.js
@@ -1,32 +1,50 @@
-const { ethers, Contract } = require("ethers");
-const NetworkInfomation = require("../src/NetworkInfomation.json");
-const ERC20MockABI = require("../../contracts/abi/");
-const { getWallet, getWalletAddress, getProvider } = require("./helper");
+import Web3 from "web3";
+const NetworkInformation = require("../lib/network-information.json");
+const ERC20MockABI = require("../../contracts/abi/ERC20Mock.json");
 
+function getRpcUrl(chainId) {
+  let rpcUrl;
+  if (chainId == 80002) {
+    rpcUrl = POLYGON_AMOY_RPC_URL;
+  } else if (chainId == 11155420) {
+    rpcUrl = OPTIMISM_SEPOLIA_RPC_URL;
+  } else if (chainId == 84532) {
+    rpcUrl = BASE_SEPOLIA_RPC_URL;
+  }
+  return rpcUrl;
+}
+
+function getProvider(chainId) {
+  const rpcUrl = getRpcUrl(chainId);
+  return new Web3(new Web3.providers.HttpProvider(rpcUrl));
+}
 async function getBalance(chainId, tokenSymbol, walletAddress) {
-  const provider = getProvider(chainId);
-  const tokenInfo = NetworkInfomation[chainId]["TOKEN"][tokenSymbol];
-  const tokenAddress = tokenInfo.address;
-  const tokenDecimals = tokenInfo.decimals;
-  const tokenContract = new Contract(tokenAddress, ERC20MockABI, provider);
-  const balance = await tokenContract.balanceOf(walletAddress);
-  const balanceFormat = ethers.utils.formatUnits(balance, tokenDecimals);
-  //console.log(`Balance of Token ${tokenSymbol} with Address ${walletAddress}: ${balanceFormat}`);
-  return balanceFormat;
+  try {
+    const web3 = getProvider(chainId);
+    const tokenInfo = NetworkInformation[chainId]["TOKEN"][tokenSymbol];
+    const tokenAddress = tokenInfo.address;
+    const tokenDecimals = tokenInfo.decimals;
+    const tokenContract = new web3.eth.Contract(ERC20MockABI, tokenAddress);
+    const balance = await tokenContract.methods.balanceOf(walletAddress).call();
+    const balanceFormat = balance / 10 ** tokenDecimals;
+    return balanceFormat.toString();
+  } catch (error) {
+    console.error(`Error fetching balance for ${tokenSymbol}:`, error);
+    return "0";
+  }
 }
 
 async function main() {
-  const walletAddress = "0xB1A296a720D9AAF5c5e9F805d8095e6d94882eE1";
-  const chainId = 84532;
+  const walletAddress = "0x57AB4e29d13E063D791Ec02116cef032314F787B";
+  const chainId = 80002;
   const wbtcBalance = await getBalance(chainId, "WBTC", walletAddress);
   const wethBalance = await getBalance(chainId, "WETH", walletAddress);
-  const uniBalance = await getBalance(chainId, "UNI", walletAddress);
-  const usdcBalance = await getBalance(chainId, "USDC", walletAddress);
-  const usdtBalance = await getBalance(chainId, "USDT", walletAddress);
   const linkBalance = await getBalance(chainId, "LINK", walletAddress);
+  const avaxBalance = await getBalance(chainId, "AVAX", walletAddress);
+  const ckesBalance = await getBalance(chainId, "CKES", walletAddress);
+  const goldBalance = await getBalance(chainId, "XAUT", walletAddress);
+  const opBalance = await getBalance(chainId, "OP", walletAddress);
 }
-
-// main();
 
 module.exports = {
   getBalance,
