a85d28179654fcb289d77073fc6bb6762b624eb5
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-09-10 17:05:06 +0300

    chore: - add readme media - add loading state for routing

--- frontend/components/app/sidebar/sidebar.tsx
+++ frontend/components/app/sidebar/sidebar.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import logo from "../../../public/logo.png";
 import Image from "next/image";
 import { MdSpaceDashboard } from "react-icons/md";
@@ -8,8 +10,22 @@ import { FaGear, FaSackDollar } from "react-icons/fa6";
 import Link from "next/link";
 import { RiMessage3Fill } from "react-icons/ri";
 import { IoLogOut } from "react-icons/io5";
-
+import { useRouter } from "next/navigation";
 function Sidebar() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const handleNavigation = (path: string) => {
+    setLoading(true);
+    router.replace(path);
+  };
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen w-full">
+        <span className="loading loading-ring loading-md"></span>
+      </div>
+    );
+  }
   return (
     <aside className="hidden lg:flex flex-col gap-48 md:w-[25%] lg:w-[20%] mt-0 bg-[#fcfcfc] shadow-md h-screen pl-2">
       <div className="flex gap-2 items-center mx-4 my-4">
@@ -18,19 +34,35 @@ function Sidebar() {
       </div>
       <div className="">
         <nav className="flex flex-col gap-2">
-          <Link href="/dashboard" className="p-4 flex gap-2 items-center my-2">
+          <Link
+            href="/dashboard"
+            className="p-4 flex gap-2 items-center my-2"
+            onClick={() => handleNavigation("/dashboard")}
+          >
             <MdSpaceDashboard />
             Dashboard
           </Link>
-          <Link href="/deposit" className="p-4 flex gap-2 items-center my-2">
+          <Link
+            href="/deposit"
+            className="p-4 flex gap-2 items-center my-2"
+            onClick={() => handleNavigation("/deposit")}
+          >
             <SiDepositphotos />
             Deposit
           </Link>
-          <Link href="/swap" className="p-4 flex gap-2 items-center my-2">
+          <Link
+            href="/swap"
+            className="p-4 flex gap-2 items-center my-2"
+            onClick={() => handleNavigation("/swap")}
+          >
             <IoIosSwap />
             Swap
           </Link>
-          <Link href="/quick" className="p-4 flex gap-2 items-center my-2">
+          <Link
+            href="/quick"
+            className="p-4 flex gap-2 items-center my-2"
+            onClick={() => handleNavigation("/quick")}
+          >
             <FaSackDollar />
             Quick Borrow
           </Link>
