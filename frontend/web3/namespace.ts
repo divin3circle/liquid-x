02e85068bbb01e5f471c25329fc93030e028bd24
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-09-11 04:10:49 +0300

    web3js

--- frontend/web3/namespace.ts
+++ frontend/web3/namespace.ts
@@ -1,16 +1,22 @@
 import { Web3 } from "web3";
 import { Chain, EnsPlugin } from "@namespace-ens/web3-plugin-ens";
 import { getProvider } from "./getBalance";
+import { AbiItem } from "web3-utils";
 
-export class EnsService {
+// Import the ABI for your CustomNamespace contract
+import CustomNamespaceABI from "../../contracts/abi/Namespace.json";
+
+export class NamespaceService {
   private web3: Web3;
   private chainId: number;
+  private customNamespaceContract: any;
 
   constructor(chainId: number) {
     const provider = getProvider(chainId);
     this.web3 = new Web3(provider);
     this.chainId = chainId;
     this.initializeEnsPlugin();
+    this.initializeCustomNamespace();
   }
 
   private initializeEnsPlugin() {
@@ -22,12 +28,24 @@ export class EnsService {
       case 11155111:
         chain = Chain.Sepolia;
         break;
+      case 11155420: // Optimism Sepolia
+        chain = Chain.Sepolia;
+        break;
       default:
         throw new Error(`Unsupported chain ID for ENS: ${this.chainId}`);
     }
     this.web3.registerPlugin(new EnsPlugin(chain));
   }
 
+  private initializeCustomNamespace() {
+    const customNamespaceAddress = "YOUR_DEPLOYED_CONTRACT_ADDRESS"; // Replace with your actual contract address
+    this.customNamespaceContract = new this.web3.eth.Contract(
+      CustomNamespaceABI as AbiItem[],
+      customNamespaceAddress
+    );
+  }
+
+  // ENS methods
   async setAddress(name: string, address: string) {
     return await this.web3.ens.setAddress(name, address);
   }
@@ -59,5 +77,40 @@ export class EnsService {
 
   async getTextRecords(name: string, recordKeys: string[]) {
     return await this.web3.ens.getTextRecords(name, recordKeys);
+  }
+
+  // Custom Namespace methods
+  async registerCustomNamespace(name: string) {
+    const accounts = await this.web3.eth.getAccounts();
+    const registrationFee = await this.customNamespaceContract.methods
+      .registrationFee()
+      .call();
+    return await this.customNamespaceContract.methods
+      .registerNamespace(name)
+      .send({
+        from: accounts[0],
+        value: registrationFee,
+      });
+  }
+
+  async transferCustomNamespace(name: string, to: string) {
+    const accounts = await this.web3.eth.getAccounts();
+    return await this.customNamespaceContract.methods
+      .transferNamespace(name, to)
+      .send({
+        from: accounts[0],
+      });
+  }
+
+  async getCustomNamespaceOwner(name: string) {
+    return await this.customNamespaceContract.methods
+      .getNamespaceOwner(name)
+      .call();
+  }
+
+  async getOwnedCustomNamespaces(address: string) {
+    return await this.customNamespaceContract.methods
+      .getOwnedNamespaces(address)
+      .call();
   }
 }
